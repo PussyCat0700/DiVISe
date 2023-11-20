7d2493a8c2974bbd604c5d915b3d75a03e431e50
1729372667@qq.com <1729372667@qq.com>
2023-11-20 22:47:28 +0800

    kaldi pitch length make-up

--- dataset/kaldi/kaldi_pitch.py
+++ dataset/kaldi/kaldi_pitch.py
@@ -8,9 +8,15 @@ def pitch_single(wav, mode, sampling_rate=16000, hop_length=160):
        corresponds to pitch and NCCF.
     """
     wav = torch.FloatTensor(wav)
+    # make up for frame_length
+    frm_length = 25  # kaldi default frame_length
+    frm_shift = hop_length / sampling_rate * 1000  # 160/16000*1000=10
+    wav_offset = int(frm_length/frm_shift)*hop_length  # 3*160
+    wav_offset = torch.zeros(*wav.shape[:-1], wav_offset)
+    wav = torch.cat((wav, wav_offset), dim=-1)
     pitch_and_nfcc = compute_kaldi_pitch(
         waveform=wav,
         sample_rate=sampling_rate,  # 16000
-        frame_shift=hop_length / sampling_rate * 1000,  # 160/16000*1000=10
+        frame_shift=frm_shift,  # 10
     )
     return pitch_and_nfcc
