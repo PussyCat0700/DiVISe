fixed normalization bug:previous versions won't get the right prosody embedding!

avhubert/avhubert_as_upstream.py
@@ -121,13 +121,13 @@ class AVHubertEncoder(nn.Module):
             "attention_dim":512,
         }
     }
-    def __init__(self, cfg, num_mels, use_prosody, size="M") -> None:
+    def __init__(self, cfg, num_mels, prosody_minmax_dict, size="M") -> None:
         super().__init__()
         self.attention_dim = self.lookup_table[size]["attention_dim"]
         self.avhubert_model = AVHubertModel(cfg=cfg)
-        self.use_prosody = use_prosody
+        self.use_prosody = prosody_minmax_dict is not None
         if self.use_prosody:
-            self.prosody_predictor = ProsodyPredictor()
+            self.prosody_predictor = ProsodyPredictor(**prosody_minmax_dict)
         self.conformer_encoder = ConformerEncoder(size)
         self.avhubert2downstream = torch.nn.Linear(768, self.attention_dim*4)
         self.attention2mel = torch.nn.Linear(self.attention_dim, num_mels)

models.py
@@ -127,9 +127,9 @@ class Generator(torch.nn.Module):
         remove_weight_norm(self.conv_post)
     
 class AVHuBERTGenerator(nn.Module):
-    def __init__(self, hifigenerator_config, avhubert_model_config, use_prosody) -> None:
+    def __init__(self, hifigenerator_config, avhubert_model_config, prosody_minmax_dict) -> None:
         super().__init__()
-        self.frontend_with_encoder = AVHubertEncoder(avhubert_model_config, hifigenerator_config.num_mels, use_prosody=use_prosody)
+        self.frontend_with_encoder = AVHubertEncoder(avhubert_model_config, hifigenerator_config.num_mels, prosody_minmax_dict=prosody_minmax_dict)
         attention_dim = self.frontend_with_encoder.attention_dim
         self.generator = Generator(hifigenerator_config, attention_dim)
     

prosody_predictor/predictor.py
@@ -11,8 +11,8 @@ class ProsodyPredictor(nn.Module):
     https://github.com/ming024/FastSpeech2/blob/d4e79eb52e8b01d24703b2dfc0385544092958f3/model/modules.py#L17C19-L17C19
     """
     # TODO: check pitch/energy min/max for LRS3
-    def __init__(self, n_bins=256, encoder_hidden=256,
-                 pitch_min=65, pitch_max=700, energy_min=0.0, energy_max=300,
+    def __init__(self, pitch_min, pitch_max, energy_min, energy_max,
+                 n_bins=256, encoder_hidden=256,
                  ):
         super().__init__()
         self.pitch_predictor = Predictor()

train.py
@@ -45,10 +45,15 @@ def train(rank, a, h, avhubert_config):
     torch.cuda.manual_seed(h.seed)
     torch.cuda.set_device(rank)  # A very strong boost. See https://github.com/jik876/hifi-gan/pull/25
     device = torch.device('cuda:{:d}'.format(rank))
-    
+    prosody_minmax_dict = {
+        "pitch_min":65,
+        "pitch_max":700,
+        "energy_min":0.0,
+        "energy_max":150,
+    } if a.prosody else None
     generator = AVHuBERTGenerator(hifigenerator_config=h,
                                   avhubert_model_config=avhubert_config["model"], 
-                                  use_prosody=a.prosody,
+                                  prosody_minmax_dict=prosody_minmax_dict,
                                   ).to(device)
     mpd = MultiPeriodDiscriminator().to(device)
     msd = MultiScaleDiscriminator().to(device)
@@ -161,8 +166,6 @@ def train(rank, a, h, avhubert_config):
                     return (x - x.mean(dim=-1, keepdim=True))/(m+x.std(dim=-1, keepdim=True))
                 energy_targets = y_dict["energy"].to(device)
                 pitch_targets = avhubert_source_batch["pitch"].to(device)
-                energy_targets = normalize_prosody(energy_targets)
-                pitch_targets = normalize_prosody(pitch_targets)
                 if a.real_prosody:
                     # keys are param names of forward func of ProsodyPredictor
                     prosody_target["pitch_target"] = pitch_targets
@@ -173,6 +176,11 @@ def train(rank, a, h, avhubert_config):
             if a.prosody:
                 pitch_predictions = generator_out["prosody"]["pitch_pred"]
                 energy_predictions = generator_out["prosody"]["energy_pred"]
+                
+                energy_targets = normalize_prosody(energy_targets)
+                pitch_targets = normalize_prosody(pitch_targets)
+                pitch_predictions = normalize_prosody(pitch_predictions)
+                energy_predictions = normalize_prosody(energy_predictions)
 
                 pitch_loss = F.mse_loss(pitch_predictions.masked_select(~mel_padding_mask), pitch_targets.masked_select(~mel_padding_mask))
                 energy_loss = F.mse_loss(energy_predictions.masked_select(~mel_padding_mask), energy_targets.masked_select(~mel_padding_mask))