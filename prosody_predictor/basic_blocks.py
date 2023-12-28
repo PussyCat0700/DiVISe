1. fixed kmeans_target None error in unit classification mode 2. fixed masking prob computation in HuBERTRepresentationPredictor. init_p no longer gets ignored. 3. allows None mask in predictor

prosody_predictor/basic_blocks.py
@@ -46,7 +46,6 @@ class Predictor(nn.Module):
             y = y.squeeze(-1)  # (B, T, 1)->(B, T) 
         elif mask is not None:
             mask = mask.unsqueeze(-1)  # unsqueezed to suit input x of shape (B, T, C)
-        mask = mask.eq(0)
-        if mask is not None:
+            mask = mask.eq(0)
             y = y.masked_fill(mask, 0.0)
         return y

prosody_predictor/predictor.py
@@ -186,7 +186,7 @@ class HuBERTRepresentationPredictor(nn.Module):
     
     def reset_prob(self, current_step, total_steps):
         self.k = (self.stop_p - self.init_p) / total_steps
-        self.prob = self.k * current_step
+        self.prob = self.k * current_step + self.init_p
         assert 0.0<=self.prob<=1.0, f"got {self.prob}"
         
     def forward(self, 